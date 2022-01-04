4e0d681dad2d6dd8d6c4ea2223ed35a3ab55277a
ayoder243 <ayoder243@gmail.com>
2022-01-04 14:00:53 -0800

    Updates

---
+++ precache-manifest.2f233046f3f86ad936d66f3e97e4f5b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ae1bac56b61a6702e64",
+    "url": "/robofriends/static/css/main.205fa07d.chunk.css"
+  },
+  {
+    "revision": "5ae1bac56b61a6702e64",
+    "url": "/robofriends/static/js/main.5ae1bac5.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "e90267afff6787b73212",
+    "url": "/robofriends/static/css/2.c4bcda98.chunk.css"
+  },
+  {
+    "revision": "e90267afff6787b73212",
+    "url": "/robofriends/static/js/2.e90267af.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "80ef079eb224b7a23d6ad11f95169b1a",
+    "url": "/robofriends/index.html"
+  }
+];
